8f04526464ede65a2e0795baf40f9945adf16b9b
fernicortlo <a01026613@tec.mx>
2023-04-11 14:33:02 -0600

    casi lloro con el api

--- node_projects/hello_server/app.js
+++ node_projects/hello_server/app.js
@@ -1,11 +1,31 @@
 import express from 'express';
-
+import fs from 'fs';
 const app = express();
+//const port = 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send('Hello World!')
-})   
+app.use(express.static('./public'))
+app.get('/', (req, res)=>
+{
+    fs.readFile('./public/html/index.html', 'utf8', 
+    (err, html) => {
+        if(err)
+        {
+            res.status(500).send('There was an error: ' + err)
+            return 
+        }
+        
+        console.log("Sending page...")
+        res.send(html)
+        console.log("Page sent!")
+    })
+})
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+
+ app.get('/api/hello', (req, res) => {
+   res.send('Hello World!')
+ })   
+
+ app.listen(3000, () => {
+     console.log('Example app listening on port 3000!');
+ });
+
---
+++ node_projects/hello_server/public/js/index.js
@@ -0,0 +1,12 @@
+const request = await fetch('http://127.0.0.1.3000/api/hello', {
+    method: 'GET',
+})
+
+if(response.ok)
+{
+    const message = await response.text()
+    console.log(message)
+
+    const resultDiv = document.getElementById('response-placeholder')
+    resultDiv.innerHTML = message
+}
